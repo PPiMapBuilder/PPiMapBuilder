916effd30a19fbd2341c187024aed7401ec26574
dsi-nomistech <dsi-nomistech@Kevin-PC>
2013-12-10 18:22:34 +0100

    add complete support of PSICQUIC XML registry

--- src/main/java/tk/nomis_tech/ppimapbuilder/util/PsicquicRegistry.java
+++ src/main/java/tk/nomis_tech/ppimapbuilder/util/PsicquicRegistry.java
@@ -3,7 +3,9 @@ package tk.nomis_tech.ppimapbuilder.util;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -25,7 +27,58 @@ public class PsicquicRegistry {
      */
     private final ArrayList<PsicquicService> services = new ArrayList<PsicquicService>();
 
-    ;
+    public int size() {
+        return services.size();
+    }
+
+    public boolean isEmpty() {
+        return services.isEmpty();
+    }
+
+    public boolean contains(Object o) {
+        return services.contains(o);
+    }
+
+    public PsicquicService get(int index) {
+        return services.get(index);
+    }
+
+    public boolean add(PsicquicService e) {
+        return services.add(e);
+    }
+
+    public PsicquicService remove(int index) {
+        return services.remove(index);
+    }
+
+    public boolean remove(Object o) {
+        return services.remove(o);
+    }
+
+    public boolean remove(String name) {
+        for (PsicquicService psicquicService : services) {
+            if (psicquicService.getName().equalsIgnoreCase(name)) {
+                return services.remove(psicquicService);
+            }
+        }
+        return false;
+    }
+
+    public void clear() {
+        services.clear();
+    }
+
+    public ListIterator<PsicquicService> listIterator(int index) {
+        return services.listIterator(index);
+    }
+
+    public ListIterator<PsicquicService> listIterator() {
+        return services.listIterator();
+    }
+
+    public Iterator<PsicquicService> iterator() {
+        return services.iterator();
+    }
 
     /**
      * Create the PsicquicRegistry object.
@@ -54,25 +107,30 @@ public class PsicquicRegistry {
      * @throws MalformedURLException
      * @throws IOException
      */
-    private void retrieveServices() throws IOException {
+    public void retrieveServices() throws IOException {
         this.services.clear();
 
         Document doc = Jsoup.connect(registryXmlUrl).get();
         for (Element e : doc.select("service")) {
-            services.add(new PsicquicService(e.select("name").first().text(), e.select("restUrl").first().text()));
+
+            List<String> tags = new ArrayList<String>();
+            for (Iterator<Element> it = e.select("tag").iterator(); it.hasNext();) {
+                Element elt = it.next();
+                tags.add(elt.text());
+            }
+
+            services.add(
+                    new PsicquicService(
+                            e.select("name").first().text(),
+                            e.select("soapUrl").first().text(),
+                            e.select("restUrl").first().text(),
+                            e.select("active").first().text(),
+                            e.select("count").first().text(),
+                            e.select("version").first().text(),
+                            e.select("organizationUrl").first().text(),
+                            e.select("restricted").first().text(),
+                            tags
+                    ));
         }
-
-    }
-
-    public int size() {
-        return services.size();
-    }
-
-    public boolean isEmpty() {
-        return services.isEmpty();
-    }
-
-    public boolean add(PsicquicService e) {
-        return services.add(e);
     }
 }
--- src/main/java/tk/nomis_tech/ppimapbuilder/util/PsicquicService.java
+++ src/main/java/tk/nomis_tech/ppimapbuilder/util/PsicquicService.java
@@ -1,25 +1,111 @@
 package tk.nomis_tech.ppimapbuilder.util;
 
+import java.util.Iterator;
+import java.util.List;
+
 /**
  *
  * @author Kévin Gravouil
  */
 public class PsicquicService {
 
-    private final String name;
-    private final String url;
+    protected String name;
+    protected String soapUrl;
+    protected String restUrl;
+    protected boolean active;
+    protected long count;
+    protected String version;
+    protected String organizationUrl;
+    protected boolean restricted;
+    protected List<String> tags;
 
-    public PsicquicService(String name, String url) {
+    public PsicquicService(String name, String soapUrl, String restUrl, String active, String count, String version, String organizationUrl, String restricted, List<String> tags) {
         this.name = name;
-        this.url = url;
+        this.soapUrl = soapUrl;
+        this.restUrl = restUrl;
+        this.active = active.equals("y");
+        this.count = Integer.parseInt(count);
+        this.version = version;
+        this.organizationUrl = organizationUrl;
+        this.restricted = restricted.equals("y");
+        this.tags = tags;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getUrl() {
-        return url;
+    public String getSoapUrl() {
+        return soapUrl;
+    }
+
+    public String getRestUrl() {
+        return restUrl;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public long getCount() {
+        return count;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public String getOrganizationUrl() {
+        return organizationUrl;
+    }
+
+    public boolean isRestricted() {
+        return restricted;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("PsicquicService{" + "name=" + name + ", soapUrl=" + soapUrl + ", restUrl=" + restUrl + ", active=" + active + ", count=" + count + ", version=" + version + ", organizationUrl=" + organizationUrl + ", restricted=" + restricted + ", tags=");
+        
+        sb.append('[');
+        for (Iterator<String> it = tags.iterator(); it.hasNext();) {
+            String t = it.next();
+            sb.append(t);
+            if(it.hasNext()) {
+                sb.append(',');
+            }
+        }
+        sb.append("]}");
+        return sb.toString();
     }
 
 }
+
+
+/*
+ <service>
+ <name>APID</name>
+ <soapUrl>
+ http://cicblade.dep.usal.es/psicquic-ws/webservices/psicquic
+ </soapUrl>
+ <restUrl>
+ http://cicblade.dep.usal.es/psicquic-ws/webservices/current/search/
+ </restUrl>
+ <restExample>
+ http://cicblade.dep.usal.es/psicquic-ws/webservices/current/search/interactor/P00533
+ </restExample>
+ <active>true</active>
+ <count>416124</count>
+ <version>1.1.5</version>
+ <organizationUrl>http://bioinfow.dep.usal.es/apid/index.htm</organizationUrl>
+ <restricted>false</restricted>
+ <tag>MI:1047</tag>
+ <tag>MI:1058</tag>
+ <tag>MI:1060</tag>
+ <tag>MI:1052</tag>
+ </service>
+ */
