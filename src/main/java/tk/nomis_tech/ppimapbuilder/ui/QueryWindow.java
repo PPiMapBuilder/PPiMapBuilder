60b8a75b40d383f604c6d0da7412a9f11a217aef
dsi-nomistech <dsi-nomistech@Kevin-PC>
2013-12-10 18:24:13 +0100

    run a query on each available psicquic services

--- src/main/java/tk/nomis_tech/ppimapbuilder/ui/QueryWindow.java
+++ src/main/java/tk/nomis_tech/ppimapbuilder/ui/QueryWindow.java
@@ -5,6 +5,9 @@ import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collection;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -14,6 +17,8 @@ import org.hupo.psi.mi.psicquic.wsclient.PsicquicSimpleClient;
 import psidev.psi.mi.tab.PsimiTabException;
 import psidev.psi.mi.tab.PsimiTabReader;
 import psidev.psi.mi.tab.model.BinaryInteraction;
+import tk.nomis_tech.ppimapbuilder.util.PsicquicRegistry;
+import tk.nomis_tech.ppimapbuilder.util.PsicquicService;
 
 /**
  * PPiMapBuilder interaction query window
@@ -30,17 +35,28 @@ public class QueryWindow extends JFrame {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                try {
-                    PsicquicSimpleClient client = new PsicquicSimpleClient("http://www.ebi.ac.uk/Tools/webservices/psicquic/intact/webservices/current/search/");
-                    PsimiTabReader mitabReader = new PsimiTabReader();
-                    InputStream result = client.getByQuery("brca2");
-                    Collection<BinaryInteraction> binaryInteractions = mitabReader.read(result);
-                    System.out.println("Interactions found: " + binaryInteractions.size());
 
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                } catch (PsimiTabException e1) {
-                    e1.printStackTrace();
+                try {
+                    // official registry
+                    PsicquicRegistry registry = new PsicquicRegistry();
+                    registry.retrieveServices();
+                    registry.remove("genemania"); // remove problematic (and dirty) DB
+
+                    for (PsicquicService service : registry.getServices()) {
+
+//                        System.out.println(service.toString());
+                        System.out.println("----- >>> " + service.getName() + "----------------------");
+                        PsicquicSimpleClient client = new PsicquicSimpleClient(service.getRestUrl());
+                        PsimiTabReader mitabReader = new PsimiTabReader();
+                        InputStream result = client.getByInteractor("P04040");
+                        Collection<BinaryInteraction> binaryInteractions = mitabReader.read(result);
+                        System.out.println("Interactions found: " + binaryInteractions.size());
+                        System.out.println("---------------------------------------");
+                    }
+                } catch (IOException ioe) {
+                    ioe.printStackTrace();
+                } catch (PsimiTabException ex) {
+                    Logger.getLogger(QueryWindow.class.getName()).log(Level.SEVERE, null, ex);
                 }
 
                 QueryWindow.this.setVisible(false);
