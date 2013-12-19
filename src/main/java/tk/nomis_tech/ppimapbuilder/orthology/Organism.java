c0336f94df69afcab555a13647f5f9477a7f3dc8
dsi-nomistech <dsi-nomistech@Kevin-PC>
2013-12-19 09:11:04 +0100

    modelize an organism as a name and NCBI taxId

---
+++ src/main/java/tk/nomis_tech/ppimapbuilder/orthology/Organism.java
@@ -0,0 +1,33 @@
+package tk.nomis_tech.ppimapbuilder.orthology;
+
+/**
+ *
+ * @author dsi-nomistech
+ */
+class Organism {
+
+	private String name;
+	private Integer ncbiTaxId;
+
+	public Organism(String name, Integer ncbiTaxId) {
+		this.name = name;
+		this.ncbiTaxId = ncbiTaxId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getNcbiTaxId() {
+		return ncbiTaxId;
+	}
+
+	public void setNcbiTaxId(Integer ncbiTaxId) {
+		this.ncbiTaxId = ncbiTaxId;
+	}
+
+}
