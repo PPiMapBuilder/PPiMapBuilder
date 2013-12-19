b766ad02b447bf4133afa07af10f12c50230d892
dsi-nomistech <dsi-nomistech@Kevin-PC>
2013-12-19 09:05:30 +0100

    uniprot id class, with pattern verification

---
+++ src/main/java/tk/nomis_tech/ppimapbuilder/orthology/UniprotId.java
@@ -0,0 +1,67 @@
+package tk.nomis_tech.ppimapbuilder.orthology;
+
+/**
+ *
+ * @author Kevin Gravouil
+ */
+class UniprotId {
+
+	/**
+	 * Exception throwed when Uniprot ID is invalid.
+	 */
+	public class BadUniprotIdFormatException extends Exception {
+
+		public BadUniprotIdFormatException() {
+		}
+
+		public BadUniprotIdFormatException(String message) {
+			super(message);
+		}
+
+	}
+
+	/**
+	 * Uniprot ID pattern. According to
+	 * http://www.ebi.ac.uk/miriam/main/export/xml/
+	 */
+	public final static String pattern = "^([A-N,R-Z][0-9][A-Z][A-Z, 0-9][A-Z, 0-9][0-9])|([O,P,Q][0-9][A-Z, 0-9][A-Z, 0-9][A-Z, 0-9][0-9])(\\.\\d+)?$";
+	/**
+	 * The Uniprot ID.
+	 */
+	private String uniprotId;
+
+	/**
+	 * Construc a Uniprot ID object if the given string is valid or throw an
+ BadUniprotIdFormatException.
+	 *
+	 * @param uniprotId
+	 * @throws
+	 * tk.nomis_tech.ppimapbuilder.orthology.UniprotId.BadUniprotIdFormatException
+	 */
+	public UniprotId(String uniprotId) throws BadUniprotIdFormatException {
+		if (uniprotId.matches(pattern)) {
+			this.uniprotId = uniprotId;
+		} else {
+			throw new BadUniprotIdFormatException("Id is not Uniprot-like");
+		}
+	}
+
+	public String getUniprotId() {
+		return uniprotId;
+	}
+
+	public void setUniprotId(String uniprotId) {
+		this.uniprotId = uniprotId;
+	}
+
+	/**
+	 * Test if a given string matches the Uniprot ID pattern.
+	 *
+	 * @param uniprotId
+	 * @return boolean
+	 */
+	public static boolean isValid(String uniprotId) {
+		return uniprotId.matches(pattern);
+	}
+
+}
