bfb3d6da71f14d7b307b15b8178020fa4d92741d
dsi-nomistech <dsi-nomistech@Kevin-PC>
2013-12-19 16:41:41 +0100

    improve data retrieving and make it static

--- src/main/java/tk/nomis_tech/ppimapbuilder/orthology/InParanoidClient.java
+++ src/main/java/tk/nomis_tech/ppimapbuilder/orthology/InParanoidClient.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -16,85 +14,100 @@ import org.jsoup.nodes.Element;
  */
 public class InParanoidClient {
 
-	private String url;
-	private String refID;
-	private String orthoID;
-	private HashMap<String, String> species = new HashMap<String, String>();
+	private static String url;
+	private static HashMap<String, String> species = new HashMap<String, String>();
 
-	public InParanoidClient() {
-		this.testClient();
+	private InParanoidClient() {
 	}
 
-	public String get(String key) {
+	public static String get(String key) {
 		return species.get(key);
 	}
 
-	public boolean containsKey(String key) {
+	public static boolean containsKey(String key) {
 		return species.containsKey(key);
 	}
 
-	public String put(String key, String value) {
+	public static String put(String key, String value) {
 		return species.put(key, value);
 	}
 
-	public String remove(String key) {
+	public static String remove(String key) {
 		return species.remove(key);
 	}
 
-	public void clear() {
+	public static void clear() {
 		species.clear();
 	}
 
-	public boolean containsValue(String value) {
+	public static boolean containsValue(String value) {
 		return species.containsValue(value);
 	}
 
-	public Set<String> keySet() {
+	public static Set<String> keySet() {
 		return species.keySet();
 	}
 
-	private void testClient() {
-		
-		System.out.println("");
+	public static void getOrthologUniprotId(String uniprotId, Integer orthologNcbiTaxId) {
+		try {
+			InParanoidClient.url = "http://inparanoid.sbc.su.se/cgi-bin/gene_search.cgi?"
+				+ "idtype=proteinid;"
+				+ "all_or_selection=selection;"
+				+ "scorelimit=0.05;"
+				+ "rettype=xml;"
+				+ "id=" + uniprotId + ";"
+				+ "specieslist=" + Ortholog.translateTaxID(orthologNcbiTaxId) + ";";
 
-		ArrayList<String> ids = new ArrayList();
-		ids.add("P07900");
-		ids.add("P04040");
-		ids.add("Q04565");
-		ids.add("Q9C519");
-		ids.add("P69891");
-		ids.add("P02091");
-		ids.add("P02008");
+			Document doc = Jsoup.connect(InParanoidClient.url).get();
 
-		ArrayList<String> notfound = new ArrayList();
+			for (Element e : doc.select("protein")) {
+//				Integer tmp = ;
 
-		for (String id : ids) {
-			System.out.println("== " + id);
-			try {
-				this.url = "http://inparanoid.sbc.su.se/cgi-bin/gene_search.cgi?"
-					+ "idtype=proteinid;"
-					+ "all_or_selection=selection;"
-					+ "scorelimit=0.05;"
-					+ "rettype=xml;"
-					+ "id=" + id + ";"
-					+ "specieslist=255;";
+//				System.out.println("### " + tmp + " // given:" + orthologNcbiTaxId);
+				if (Integer.valueOf(e.attr("speclink").replace("http://www.uniprot.org/taxonomy/", "")).equals(orthologNcbiTaxId)) {
 
-				Document doc = Jsoup.connect(this.url).get();
-				for (Element e : doc.select("protein")) {
-					if (Double.valueOf(e.attr("score")) >= 0.95) {
-						System.out.println("\tSPECIE:" + e.attr("speclong"));
-						System.out.println("\tPTN_ID:" + e.attr("prot_id"));
-						System.out.println("\tSCORE:" + e.attr("score"));
-						System.out.println("");
+					if (Double.valueOf(e.attr("score")) >= 0.85) { // TODO: review this to pick only the best one ?
+//						System.out.println("SPECIE: " + e.attr("speclong"));
+//						System.out.println("TAXID: " + e.attr("speclink").replace("http://www.uniprot.org/taxonomy/", ""));
+						System.out.println("Orthologous protein id: " + e.attr("prot_id") + " (score: " + e.attr("score") + ")");
 					}
 				}
-			} catch (IOException ex) {
-				notfound.add(id);
 			}
+		} catch (IOException ex) {
+			System.out.println("No ortholgs found.");
+		}
+	}
+
+	public static void testClient() {
+
+		ArrayList<Integer> orthotaxids = new ArrayList<Integer>() {
+			{
+				add(10090);
+				add(3702);
+				add(6239);
+			}
+		};
+
+		ArrayList<String> ids = new ArrayList() {
+			{
+				add("P07900");
+				add("P04040");
+				add("Q04565");
+				add("Q9C519");
+				add("P69891");
+				add("P02091");
+				add("P02008");
+			}
+		};
+		for (Integer orthotaxid : orthotaxids) {
+			System.out.println("--------[" + orthotaxid + "]-----------------------------------");
+			// tests
+			for (String id : ids) {
+				System.out.println("==" + id);
+				InParanoidClient.getOrthologUniprotId(id, orthotaxid);
+			}
+
 		}
 
-		if (!notfound.isEmpty()) {
-			Logger.getLogger(InParanoidClient.class.getName()).log(Level.WARNING, "no ortholog found for {0}", notfound.toString());
-		}
 	}
 }
