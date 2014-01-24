67123a6c15ccb95c3f42283442e8b449ef2c5cf8
pidupuis <pmj.dupuis@gmail.com>
2014-01-24 17:47:38 +0100

    Update PsicquicRegistry.java

--- src/main/java/tk/nomis_tech/ppimapbuilder/util/PsicquicRegistry.java
+++ src/main/java/tk/nomis_tech/ppimapbuilder/util/PsicquicRegistry.java
@@ -13,15 +13,13 @@ import org.jsoup.nodes.Element;
 /**
  * Retrieve the PSICQUIC registry in a very simple way. TODO: Use Registry and
  * XML file
- *
- * @author Kevin Gravouil, Guillaume Cornut
  */
 public class PsicquicRegistry {
 
 	/**
 	 * The base URL for PSICQUIC registry.
 	 */
-	private static final String registryXmlUrl = "http://www.ebi.ac.uk/Tools/webservices/psicquic/registry/registry?action=STATUS&format=xml";
+	private static final String registryXmlUrl = "http://www.ebi.ac.uk/Tools/webservices/psicquic/registry/registry?action=ACTIVE&format=xml";
 	/**
 	 * Structure to store service names and services URLs.
 	 */
@@ -126,7 +124,7 @@ public class PsicquicRegistry {
 					e.select("restUrl").first().text(),
 					e.select("active").first().text(),
 					e.select("count").first().text(),
-					e.select("version").equals("")?e.select("version").first().text():"",
+					e.select("version").first().text(),
 					e.select("organizationUrl").first().text(),
 					e.select("restricted").first().text(),
 					tags
