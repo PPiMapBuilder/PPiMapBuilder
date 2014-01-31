52bf2e22b6accc4c855f29509778b0aa0e4f17a9
keuv-gravouil <k.gravouil@gmail.com>
2014-01-31 18:51:11 +0100

    refactoring

--- src/main/java/tk/nomis_tech/ppimapbuilder/ui/PMBResultPanel.java
+++ src/main/java/tk/nomis_tech/ppimapbuilder/ui/PMBResultPanel.java
@@ -1,51 +1,55 @@
 package tk.nomis_tech.ppimapbuilder.ui;
 
+import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
+
+import javax.swing.BoxLayout;
 import javax.swing.Icon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeNode;
+
 import org.cytoscape.application.swing.CytoPanelComponent;
 import org.cytoscape.application.swing.CytoPanelName;
 
-public class PMBResultPanel extends JPanel implements CytoPanelComponent {
-	
-	private final String title = "PPiMapBuilder";
-	
-	public PMBResultPanel() {
-		setPreferredSize(new Dimension(350, 1300));
-		setMinimumSize(new Dimension(250, 300));
-		JScrollPane scrollpane = new JScrollPane(this);
-		setAutoscrolls(true);
-		JLabel lbXYZ = new JLabel("This is my PMB Control Panel");
-		lbXYZ.setToolTipText("coucou tu veux voir ma mite?");
-		lbXYZ.setFont(new Font(null, Font.ITALIC, 25));
+import tk.nomis_tech.ppimapbuilder.ui.panel.ResultPanel;
 
-		
-		this.add(lbXYZ);
-		this.setVisible(true);
+public class PMBResultPanel extends ResultPanel implements CytoPanelComponent {
+
+	private final String title = "PPiMapBuilder";
+	private Font titleFont = new Font("Lucida Grande", Font.BOLD, 18);
+
+	private JPanel topPanel;
+	private JScrollPane scrollPane;
+
+	public PMBResultPanel() {
+		super();
+		setPreferredSize(new Dimension(320, 700));
 	}
-	
+
 	@Override
 	public Component getComponent() {
 		return this;
 	}
-	
+
 	@Override
 	public CytoPanelName getCytoPanelName() {
 		return CytoPanelName.EAST;
 	}
-	
+
 	@Override
 	public String getTitle() {
 		return this.title;
 	}
-	
+
 	@Override
 	public Icon getIcon() {
 		return null;
 	}
-	
+
 }
