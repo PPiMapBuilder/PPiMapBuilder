d81d038022ccf01f046326d21b74556d4eb08dd5
keuv-gravouil <k.gravouil@gmail.com>
2014-02-03 17:24:36 +0100

    enhance display & prepare for listeners

--- src/main/java/tk/nomis_tech/ppimapbuilder/ui/panel/ResultPanel.java
+++ src/main/java/tk/nomis_tech/ppimapbuilder/ui/panel/ResultPanel.java
@@ -2,12 +2,16 @@ package tk.nomis_tech.ppimapbuilder.ui.panel;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Insets;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
-import javax.swing.AbstractListModel;
+import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -21,108 +25,102 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 
+import org.cytoscape.application.swing.CytoPanelComponent;
+import org.cytoscape.application.swing.CytoPanelName;
+
 import net.miginfocom.swing.MigLayout;
 
-public class ResultPanel extends javax.swing.JPanel {
+public class ResultPanel extends javax.swing.JPanel implements CytoPanelComponent {
+
+	private final String title = "PPiMapBuilder";
 
 	/**
 	 * Creates new form ResultPanel
 	 */
 
-	ImageIcon urlIcon = new ImageIcon(getClass().getResource("external_link_icon.gif"));
+	private ImageIcon logo = new ImageIcon(getClass().getResource("logo.png"));
+
+	private ImageIcon urlIcon = new ImageIcon(getClass().getResource("external_link_icon.gif"));
 	private final JLabel lblReviewed = new JLabel(new ImageIcon(getClass().getResource("star.png")));
 	private final JLabel lblUnreviewed = new JLabel(new ImageIcon(getClass().getResource("unstar.png")));
 
+	private final JPanel mainPanel;
+
+	private boolean isReviewed = true;
+	private JLabel lblPtnname;
+	private JLabel proteinId;
+	private JLabel ecNum;
+	private JLabel organism;
+	private JLabel geneName;
+	private JTree treeOntology;
+	private JList<String> geneNameSynonyms;
+
 	public ResultPanel() {
+		setPreferredSize(new Dimension(320, 400));
 		setBorder(null);
 		setLayout(new BorderLayout(0, 0));
 
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.getVerticalScrollBar().setUnitIncrement(2000);
+		final JScrollPane scrollPane = new JScrollPane();
 		add(scrollPane, BorderLayout.CENTER);
 
-		final JPanel panel = new JPanel();
-		panel.setBorder(null);
-		scrollPane.setViewportView(panel);
-		panel.setLayout(new MigLayout("hidemode 3", "[70px:70px:70px,grow,right][grow][]", "[][][][][][::50px,grow][30px:80px,grow 66][30px:80px,grow]"));
+		mainPanel = new JPanel();
+		mainPanel.setBorder(null);
+		scrollPane.setViewportView(mainPanel);
+		mainPanel.setLayout(new MigLayout("hidemode 3", "[70px:70px:70px,grow,right][grow][]", "[][][][][][::50px][10px:n][30px:80px,grow]"));
 
-		JLabel lblPtnname = new JLabel("Catalase");
-		lblPtnname.setBorder(new EmptyBorder(3, 3, 3, 3));
-		lblPtnname.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		panel.add(lblPtnname, "cell 0 0 2 1,grow");
+//		 setDefaultView();
+		setTestProteinView();
 
-		JLabel lblUniprotId = new JLabel("Uniprot ID:");
-		panel.add(lblUniprotId, "cell 0 1,alignx left");
+		initComponents();
+	}
 
-		panel.add(lblReviewed, "cell 2 0,alignx right,aligny center");
+	private void setDefaultView() {
+		JLabel lblEmpty = new JLabel("Please select one node/protein.");
+		lblEmpty.setForeground(Color.DARK_GRAY);
+		mainPanel.add(lblEmpty, "cell 0 0 3 7,alignx center,aligny center");
+	}
 
-		JLabel lblExtLink1 = new JLabel(urlIcon);
-		lblExtLink1.setToolTipText("Access external link");
-		panel.add(lblExtLink1, "cell 2 1,alignx right,aligny center");
+	// TODO: private void setProteinView(Protein ptn) {
+	private void setTestProteinView() {
+		/*
+		 * HEADER - GENERAL INFORMATION
+		 */
+		lblPtnname = new JLabel("Catalase");
+		lblPtnname.setBorder(new EmptyBorder(3, 8, 3, 3));
+		lblPtnname.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		mainPanel.add(lblPtnname, "cell 0 0 2 1,grow");
 
-		JLabel lblExtLink2 = new JLabel(urlIcon);
-		lblExtLink2.setToolTipText("Access external link");
-		panel.add(lblExtLink2, "cell 2 2,alignx right,aligny center");
+		final JLabel lblUniprotId = new JLabel("Uniprot ID:");
+		mainPanel.add(lblUniprotId, "cell 0 1,alignx left");
 
-		JLabel lblExtLink3 = new JLabel(urlIcon);
-		lblExtLink3.setToolTipText("Access external link");
-		panel.add(lblExtLink3, "cell 2 3,alignx right,aligny center");
+		final JLabel lblEcNumber = new JLabel("EC Number:");
+		mainPanel.add(lblEcNumber, "flowx,cell 0 2,alignx left");
 
-		JLabel lblExtLink4 = new JLabel(urlIcon);
-		lblExtLink4.setToolTipText("Access external link");
-		panel.add(lblExtLink4, "cell 2 4,alignx right,aligny center");
+		final JLabel lblNewLabel = new JLabel("Organism:");
+		mainPanel.add(lblNewLabel, "cell 0 3,alignx left");
 
-		JLabel lblEcNumber = new JLabel("EC Number:");
-		panel.add(lblEcNumber, "flowx,cell 0 2,alignx left");
+		final JLabel lblGeneName = new JLabel("Gene name:");
+		mainPanel.add(lblGeneName, "flowx,cell 0 4,alignx left");
 
-		JLabel label_1 = new JLabel("1.11.1.6");
-		panel.add(label_1, "cell 1 2");
-
-		JLabel label = new JLabel();
-		panel.add(label, "cell 1 2");
-
-		JLabel lblNewLabel = new JLabel("Organism:");
-		panel.add(lblNewLabel, "cell 0 3,alignx left");
-
-		JLabel lblHomoSapiens = new JLabel("Homo Sapiens (9606)");
-		panel.add(lblHomoSapiens, "cell 1 3");
-
-		JLabel lblGeneName = new JLabel("Gene name:");
-		panel.add(lblGeneName, "flowx,cell 0 4,alignx left");
-
-		JLabel lblCat = new JLabel("CAT");
-		panel.add(lblCat, "cell 1 4");
-
-		JLabel lblSynonyms = new JLabel("Synonyms:");
-		panel.add(lblSynonyms, "flowx,cell 0 5,alignx left,aligny top");
+		final JLabel lblSynonyms = new JLabel("Synonyms:");
+		mainPanel.add(lblSynonyms, "flowx,cell 0 5,alignx left,aligny top");
 
 		final JScrollPane scrollPane_1 = new JScrollPane();
-		panel.add(scrollPane_1, "cell 1 5,grow");
-
-		final JList<String> list = new JList<String>();
-		list.setModel(new AbstractListModel() {
-			String[] values = new String[] { "toto", "itit" };
-
-			public int getSize() {
-				return values.length;
-			}
-
-			public Object getElementAt(int index) {
-				return values[index];
-			}
-		});
-		scrollPane_1.setViewportView(list);
-
-		scrollPane_1.setVisible(false);
+		mainPanel.add(scrollPane_1, "cell 1 5,grow");
 
 		final JToggleButton toggleButton = new JToggleButton("+");
 		toggleButton.setMargin(new Insets(2, 5, 2, 5));
+		mainPanel.add(toggleButton, "cell 2 5,alignx center,aligny top");
 
-		panel.add(toggleButton, "cell 2 5,alignx center,aligny top");
+		JScrollPane scrollPane_Orthologs = new JScrollPane();
+		scrollPane_Orthologs.setBorder(null);
+		mainPanel.add(scrollPane_Orthologs, "cell 0 6 3 1,grow");
 
-		JPanel panel_1 = new JPanel();
-		panel_1.setBorder(new TitledBorder(new LineBorder(new Color(180, 180, 180), 1, true), "Orthologs", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel.add(panel_1, "cell 0 6 3 1,grow");
+		final JPanel panel_Orthologs = new JPanel();
+		scrollPane_Orthologs.setViewportView(panel_Orthologs);
+		panel_Orthologs
+				.setBorder(new TitledBorder(new LineBorder(new Color(180, 180, 180), 1, true), "Orthologs", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_Orthologs.setLayout(new BoxLayout(panel_Orthologs, BoxLayout.Y_AXIS));
 
 		ItemListener itemListener = new ItemListener() {
 			@Override
@@ -136,28 +134,76 @@ public class ResultPanel extends javax.swing.JPanel {
 					toggleButton.setText("+");
 					scrollPane_1.setVisible(false);
 				}
-				panel.validate();
+				mainPanel.validate();
 			}
 		};
 		toggleButton.addItemListener(itemListener);
 
-		JLabel lblP = new JLabel("P04040");
-		panel.add(lblP, "cell 1 1");
+		final JScrollPane scrollPane_GO = new JScrollPane();
+		scrollPane_GO.setBorder(null);
+		mainPanel.add(scrollPane_GO, "cell 0 7 3 1,grow");
 
-		JScrollPane scrollPane_2 = new JScrollPane();
-		scrollPane_2.setBorder(null);
-		panel.add(scrollPane_2, "cell 0 7 3 1,grow");
+		final JPanel panel_GO = new JPanel();
+		scrollPane_GO.setViewportView(panel_GO);
+		panel_GO.setBorder(new TitledBorder(new LineBorder(new Color(180, 180, 180), 1, true), "Gene Ontology", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_GO.setLayout(new BorderLayout(0, 0));
 
-		final JPanel panel_2 = new JPanel();
-		scrollPane_2.setViewportView(panel_2);
-		panel_2.setBorder(new TitledBorder(new LineBorder(new Color(180, 180, 180), 1, true), "Gene Ontology", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel_2.setLayout(new BorderLayout(0, 0));
+		/*
+		 * VARIABLE LABELS
+		 */
+		proteinId = new JLabel("P04040");
+		mainPanel.add(proteinId, "cell 1 1");
 
-		JTree tree = new JTree();
-		tree.setRootVisible(false);
-		tree.setOpaque(false);
-		panel_2.add(tree);
-		initComponents();
+		ecNum = new JLabel("1.11.1.6");
+		mainPanel.add(ecNum, "cell 1 2");
+
+		organism = new JLabel("Homo Sapiens (9606)");
+		mainPanel.add(organism, "cell 1 3");
+
+		geneName = new JLabel("CAT");
+		mainPanel.add(geneName, "cell 1 4");
+
+		geneNameSynonyms = new JList<String>();
+		geneNameSynonyms.setModel(new DefaultListModel<String>() {
+			{
+				addElement("ototo");
+				addElement("hatikado");
+			}
+		});
+
+		scrollPane_1.setViewportView(geneNameSynonyms);
+		scrollPane_1.setVisible(false);
+
+		treeOntology = new JTree();
+		treeOntology.setRootVisible(false);
+		treeOntology.setOpaque(false);
+		panel_GO.add(treeOntology);
+
+		/*
+		 * ICONS
+		 */
+		{
+			if (isReviewed)
+				mainPanel.add(lblReviewed, "cell 2 0,alignx right,aligny center");
+			else
+				mainPanel.add(lblReviewed, "cell 2 0,alignx right,aligny center");
+
+			JLabel lblExtLinkUniprot = new JLabel(urlIcon);
+			lblExtLinkUniprot.setToolTipText("Access external link");
+			mainPanel.add(lblExtLinkUniprot, "cell 2 1,alignx right,aligny center");
+
+			JLabel lblExtLinkECnum = new JLabel(urlIcon);
+			lblExtLinkECnum.setToolTipText("Access external link");
+			mainPanel.add(lblExtLinkECnum, "cell 2 2,alignx right,aligny center");
+
+			JLabel lblExtLinkOrganism = new JLabel(urlIcon);
+			lblExtLinkOrganism.setToolTipText("Access external link");
+			mainPanel.add(lblExtLinkOrganism, "cell 2 3,alignx right,aligny center");
+
+			JLabel lblExtLinkGenename = new JLabel(urlIcon);
+			lblExtLinkGenename.setToolTipText("Access external link");
+			mainPanel.add(lblExtLinkGenename, "cell 2 4,alignx right,aligny center");
+		}
 	}
 
 	/**
@@ -186,5 +232,25 @@ public class ResultPanel extends javax.swing.JPanel {
 		j.getContentPane().add(r);
 		j.pack();
 		j.setVisible(true);
+	}
+
+	@Override
+	public Component getComponent() {
+		return this;
+	}
+
+	@Override
+	public CytoPanelName getCytoPanelName() {
+		return CytoPanelName.EAST;
+	}
+
+	@Override
+	public String getTitle() {
+		return this.title;
+	}
+
+	@Override
+	public Icon getIcon() {
+		return null;
 	}
 }
