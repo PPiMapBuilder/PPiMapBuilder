0b09ffe9c9b85c5df72d1e0b23b29713746ccd35
keuv-gravouil <k.gravouil@gmail.com>
2014-02-03 11:23:09 +0100

    delete useless code

--- src/main/java/tk/nomis_tech/ppimapbuilder/ui/PMBResultPanel.java
+++ src/main/java/tk/nomis_tech/ppimapbuilder/ui/PMBResultPanel.java
@@ -1,18 +1,9 @@
 package tk.nomis_tech.ppimapbuilder.ui;
 
-import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.Font;
 
-import javax.swing.BoxLayout;
 import javax.swing.Icon;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTree;
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.TreeNode;
 
 import org.cytoscape.application.swing.CytoPanelComponent;
 import org.cytoscape.application.swing.CytoPanelName;
@@ -21,11 +12,8 @@ import tk.nomis_tech.ppimapbuilder.ui.panel.ResultPanel;
 
 public class PMBResultPanel extends ResultPanel implements CytoPanelComponent {
 
+	private static final long serialVersionUID = -6177714676582301901L;
 	private final String title = "PPiMapBuilder";
-	private Font titleFont = new Font("Lucida Grande", Font.BOLD, 18);
-
-	private JPanel topPanel;
-	private JScrollPane scrollPane;
 
 	public PMBResultPanel() {
 		super();
