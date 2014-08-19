95ebb983ee62afc41e56308364b91e3347968708
pidupuis <pmj.dupuis@gmail.com>
2014-08-19 21:26:48 +0200

    Retrieve list of major GO in the network

--- src/main/java/ch/picard/ppimapbuilder/layout/PMBGOSlimLayoutTask.java
+++ src/main/java/ch/picard/ppimapbuilder/layout/PMBGOSlimLayoutTask.java
@@ -1,5 +1,11 @@
 package ch.picard.ppimapbuilder.layout;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.TreeMap;
+
 import org.cytoscape.model.CyNetwork;
 import org.cytoscape.model.CyNode;
 import org.cytoscape.model.CyTable;
@@ -25,16 +31,28 @@ public class PMBGOSlimLayoutTask extends AbstractTask {
 	public void run(TaskMonitor arg0) throws Exception {
 		System.out.println("PMB Layout Task");
 		
-		// TODO : generate list of major GO in the network
+		// Generate list of major GO in the network
 		CyTable nodeTable = view.getModel().getDefaultNodeTable();
 		
+		ArrayList<String> fullListOfGOs = new ArrayList<String>(); // Stores every GO from the network
 		for (CyNode n : network.getNodeList()) {
-			System.out.print(network.getRow(n).get("uniprot_id", String.class)+ " -> ");
+			//System.out.print(network.getRow(n).get("uniprot_id", String.class)+ " -> ");
 			for (String s : network.getRow(n).getList("go_slim", String.class)) {
-				System.out.print(s+",");
+				fullListOfGOs.add(s);
 			}
-			System.out.println();
 		}
+		
+		LinkedHashMap<String, Integer> goOccurrences = new LinkedHashMap<String, Integer>(); // Stores the number of occurrences for each GO
+		for (String go : fullListOfGOs) {
+			goOccurrences.put(go, Collections.frequency(fullListOfGOs, go));
+		}
+		//System.out.println(goOccurrences);
+
+        ValueComparator bvc =  new ValueComparator(goOccurrences);
+        TreeMap<String,Integer> sortedGoOccurrences = new TreeMap<String,Integer>(bvc); // Stores the GO ordered by amount
+		sortedGoOccurrences.putAll(goOccurrences);
+		System.out.println(sortedGoOccurrences);
+        
 		
 		// TODO : assign one major GO for each prot
 		
@@ -50,4 +68,20 @@ public class PMBGOSlimLayoutTask extends AbstractTask {
 	
 				
 
+}
+
+class ValueComparator implements Comparator<String> {
+
+	LinkedHashMap<String, Integer> base;
+    public ValueComparator(LinkedHashMap<String, Integer> base) {
+        this.base = base;
+    }
+   
+    public int compare(String a, String b) {
+        if (base.get(a) >= base.get(b)) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
 }
