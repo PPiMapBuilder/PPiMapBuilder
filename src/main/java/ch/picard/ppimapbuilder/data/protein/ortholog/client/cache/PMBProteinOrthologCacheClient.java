b70fddf7641123781cbf28fabfe228b53d074e0c
pidupuis <pierredupuis@openmailbox.org>
2015-04-30 00:34:19 +0200

    Add GPL license

--- src/main/java/ch/picard/ppimapbuilder/data/protein/ortholog/client/cache/PMBProteinOrthologCacheClient.java
+++ src/main/java/ch/picard/ppimapbuilder/data/protein/ortholog/client/cache/PMBProteinOrthologCacheClient.java
@@ -1,3 +1,23 @@
+/*   
+ * This file is part of PPiMapBuilder.
+ *
+ * PPiMapBuilder is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PPiMapBuilder is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PPiMapBuilder.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Copyright 2015 Echeverria P.C., Dupuis P., Cornut G., Gravouil K., Kieffer A., Picard D.
+ * 
+ */    	
+    
 package ch.picard.ppimapbuilder.data.protein.ortholog.client.cache;
 
 import ch.picard.ppimapbuilder.data.protein.ortholog.client.AbstractProteinOrthologClient;
