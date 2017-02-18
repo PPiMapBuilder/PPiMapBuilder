3e850ba4384df5468ae42a94e2db34ab4de32250
pidupuis <pidupuis@protonmail.com>
2017-02-18 12:52:35 +0100

    feat(credits): update credits

--- src/main/java/ch/picard/ppimapbuilder/ui/credits/CreditFrame.java
+++ src/main/java/ch/picard/ppimapbuilder/ui/credits/CreditFrame.java
@@ -74,11 +74,11 @@ public class CreditFrame extends JFrame {
 
 		// PPiMapBuilder email
 		JHyperlinkLabel lblEmail = new JHyperlinkLabel(openBrowser);
-		lblEmail.setText("<ppimapbuilder@gmail.com>");
+		lblEmail.setText("<pablo.echeverria@unige.ch>");
 		lblEmail.setToolTipText("Send an email");
 		lblEmail.makeClickable();
 		try {
-			lblEmail.setUri(new URI("mailto:ppimapbuilder@gmail.com"));
+			lblEmail.setUri(new URI("mailto:pablo.echeverria@unige.ch"));
 		} catch (URISyntaxException e) {
 			e.printStackTrace();
 		}
@@ -101,51 +101,43 @@ public class CreditFrame extends JFrame {
 		lblPicardLabLogo.setCursor(new Cursor(Cursor.HAND_CURSOR));
 		container.add(lblPicardLabLogo, "cell 2 7,alignx center");
 
-		// Developpers
-		JLabel lblAuthors = new JLabel("Developpers:");
-		lblAuthors.setFont(new Font("Lucida Grande", Font.BOLD, 12));
-		container.add(lblAuthors, "cell 0 9 3 1,alignx center");
-		
-		// Guillaume Cornut
-		JLabel lblGuillaumeCornut = new JLabel("Guillaume Cornut");
-		container.add(lblGuillaumeCornut, "cell 0 10,alignx center");
+		// Project leaders
+		JLabel lblProjectLeaders = new JLabel("Project leaders:");
+		lblProjectLeaders.setFont(new Font("Lucida Grande", Font.BOLD, 12));
+		container.add(lblProjectLeaders, "cell 0 10 3 0,alignx center");
 
 		// Pierre Dupuis
 		JLabel lblPierreDupuis = new JLabel("Pierre Dupuis");
-		container.add(lblPierreDupuis, "cell 2 10,alignx center");
+		container.add(lblPierreDupuis, "cell 0 11,alignx center");
+
+		// Guillaume Cornut
+		JLabel lblGuillaumeCornut = new JLabel("Guillaume Cornut");
+		container.add(lblGuillaumeCornut, "cell 2 11,alignx center");
+
+		// Developpers
+		JLabel lblAuthors = new JLabel("Developpers:");
+		lblAuthors.setFont(new Font("Lucida Grande", Font.BOLD, 12));
+		container.add(lblAuthors, "cell 0 13 3 0,alignx center");
+		
+		// Julien Fleury
+		JLabel lblJulienFleury = new JLabel("Julien Fleury");
+		container.add(lblJulienFleury, "cell 0 14,alignx center");
 
 		// Kévin Gravouil
 		JLabel lblKvinGravouil = new JLabel("Kévin Gravouil");
-		container.add(lblKvinGravouil, "cell 0 11,alignx center");
+		container.add(lblKvinGravouil, "cell 1 14,alignx center");
 		
 		// Armelle Kieffer
 		JLabel lblArmelleKieffer = new JLabel("Armelle Kieffer");
-		container.add(lblArmelleKieffer, "cell 2 11,alignx center");
-		
-		// UnivPoitiers logo
-		JHyperlinkLabel lblUnivPoitiersLogo = new JHyperlinkLabel(openBrowser);
-		try {
-			lblUnivPoitiersLogo.setIcon(new ImageIcon(CreditFrame.class.getResource("univ-poitiers.png")));
-		} catch (Exception e) {
-			lblUnivPoitiersLogo.setText("University of Poitiers");
-		}
-		lblUnivPoitiersLogo.setToolTipText("Access to master's degree details [fr]");
-		lblUnivPoitiersLogo.makeClickable();
-		try {
-			lblUnivPoitiersLogo.setUri(new URI("http://sfa.univ-poitiers.fr/biosante/spip.php?rubrique86"));
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		}
-		lblUnivPoitiersLogo.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		container.add(lblUnivPoitiersLogo, "cell 0 12 3 1,alignx center");
+		container.add(lblArmelleKieffer, "cell 2 14,alignx center");
 		
 		// Paper
 		JLabel lblPaperTitle = new JLabel("Original workflow:");
 		lblPaperTitle.setFont(new Font("Lucida Grande", Font.BOLD, 12));
-		container.add(lblPaperTitle, "cell 0 13 3 1,alignx center");
+		container.add(lblPaperTitle, "cell 0 16 3 0,alignx center");
 
 		JLabel lblPaper = new JLabel("Echeverría, P. C. et al. (2011), PLoS ONE 6, e26044");
-		container.add(lblPaper, "cell 0 14 3 1,alignx center");
+		container.add(lblPaper, "cell 0 17 3 1,alignx center");
 		JHyperlinkLabel lblOpenAccess = new JHyperlinkLabel(openBrowser);
 		lblOpenAccess.setText("[Open Access]");
 		lblOpenAccess.setToolTipText("http://www.plosone.org/article/info%3Adoi%2F10.1371%2Fjournal.pone.0026044");
@@ -158,7 +150,7 @@ public class CreditFrame extends JFrame {
 		lblOpenAccess.setCursor(new Cursor(Cursor.HAND_CURSOR));
 		lblOpenAccess.setForeground(Color.GRAY);
 		lblOpenAccess.setFont(new Font("Lucida Grande", Font.PLAIN, 11));
-		container.add(lblOpenAccess, "cell 0 15 3 1,alignx center");
+		container.add(lblOpenAccess, "cell 0 18 3 1,alignx center");
 		
 		JScrollPane jsp = new JScrollPane(container);
 		getContentPane().add(jsp);
